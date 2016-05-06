898d8f11275edf9ae1e86d78deca9c2b6b7bfac1
Tim Smith <tsontario@gmail.com>
2016-05-06 01:44:51 -0400

    Build out basic views, set up basic server and routing

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var http = require('http');
 var router = require('./router');
 
 var server = http.createServer(function(request, response) {
-    
     router.home(request, response);
+    router.result(request, response);
 });
 
 server.listen(3000);
+
+//TODO Write HTML for all form pieces
--- router.js
+++ router.js
@@ -2,16 +2,32 @@
  * Created by timothysmith on 2016-05-05.
  */
 var lorem = require('lorem-ipsum');
+var fs = require('fs');
+var url = require('url');
 
 var home = function(request, response) {
     if (request.url == "/") {
-        if (request.method.toLocaleLowerCase() === 'get') {
-            response.write("Hello Worldlier!");
+        response.write("Hello World");
+        response.write(arguments.toString());
+        response.end();
+    }
+};
+
+// TODO Routing call not working
+var result = function(request, response) {
+    if (request.url.match("result")) {
+        response.writeHead(200, {"Content-type": 'text/html'});
+        console.log("Writing");
+        for (var key in request) {
+            console.log(key);
             
-            response.end();
         }
+        var page = fs.readFile('views/result.html');
+        response.write(page);
+        response.end();
     }
 };
 
 
 module.exports.home = home;
+module.exports.result = result;
