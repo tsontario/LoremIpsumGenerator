6f05edff185a30e301fa604c387294d493815b87
Tim Smith <tsontario@gmail.com>
2016-05-09 14:44:41 -0400

    Functionality added to accept user queries and output correct data. TODO layout, design, and error checking

--- router.js
+++ router.js
@@ -10,6 +10,19 @@ var query = require('querystring');
 
 var htmlContent = {'Content-type' : 'text/html'};
 
+
+function generateLorem(query) {
+    var result = '';
+    result += "<div class='lorem'>";
+    result += lorem({
+        units: query["organize_by"],
+        count: query["number_of"],
+        format: "html"
+    });
+    result += "</div>";
+    return result;
+}
+
 var home = function(request, response) {
     if (request.url == "/") {
 
@@ -32,9 +45,12 @@ var result = function(request, response) {
         request.on('data', function(chunk) {
             body += chunk;
         }).on('end', function() {
-            console.log(url.parse(request.url, true).query);
+            renderer.rend(['header'], response);
+            var query = (url.parse(request.url, true).query);
+            response.write(generateLorem(query));
             response.write(body);
-            response.end("Done");
+            renderer.rend(['footer'], response);
+            response.end();
         }).on('error', function(error) {
             console.log(error);
         });
